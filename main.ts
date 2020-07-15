e86003dadeb1ae204dade8fe2f6a9023dfac006a
Juan Antonio Cortés <jacortesm@gmail.com>
2020-07-15 21:53:14 +0200

    Control de encendido de luces con el mando. Control de luz diurna.

--- main.ts
+++ main.ts
@@ -11,7 +11,6 @@ function derecha (texto: string) {
     maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
 }
 maqueen.IR_callbackUser(function (ir) {
-    basic.showNumber(ir)
     if (ir == 64) {
         alante("")
     }
@@ -28,7 +27,20 @@ maqueen.IR_callbackUser(function (ir) {
         derecha("")
     }
     if (ir == 114) {
+        rgbEstaEncendido = 1
         rgb.showColor(neopixel.colors(NeoPixelColors.Red))
+    }
+    if (ir == 113) {
+        rgbEstaEncendido = 1
+        rgb.showColor(neopixel.colors(NeoPixelColors.Green))
+    }
+    if (ir == 99) {
+        rgbEstaEncendido = 1
+        rgb.showColor(neopixel.colors(NeoPixelColors.Yellow))
+    }
+    if (ir == 97) {
+        rgbEstaEncendido = 1
+        rgb.showColor(neopixel.colors(NeoPixelColors.Blue))
     }
 })
 function alante (texto: string) {
@@ -44,6 +56,7 @@ function alante (texto: string) {
     maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
 }
 function parar (texto: string) {
+    rgbEstaEncendido = 0
     maqueen.motorStop(maqueen.Motors.All)
     rgb.showColor(neopixel.colors(NeoPixelColors.Black))
     basic.showLeds(`
@@ -55,6 +68,14 @@ function parar (texto: string) {
         `)
     maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
     maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
+    basic.pause(2000)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
 }
 function atras (texto: string) {
     basic.showLeds(`
@@ -81,27 +102,30 @@ function izquierda (texto: string) {
     maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
     maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
 }
+let rgbEstaEncendido = 0
 let rgb: neopixel.Strip = null
 rgb = neopixel.create(DigitalPin.P15, 4, NeoPixelMode.RGB)
 basic.forever(function () {
-    if (input.lightLevel() <= 55) {
-        rgb.showColor(neopixel.colors(NeoPixelColors.Red))
-        basic.pause(1000)
-        rgb.showColor(neopixel.colors(NeoPixelColors.Orange))
-        basic.pause(1000)
-        rgb.showColor(neopixel.colors(NeoPixelColors.Yellow))
-        basic.pause(1000)
-        rgb.showColor(neopixel.colors(NeoPixelColors.Green))
-        basic.pause(1000)
-        rgb.showColor(neopixel.colors(NeoPixelColors.Blue))
-        basic.pause(1000)
-        rgb.showColor(neopixel.colors(NeoPixelColors.Indigo))
-        basic.pause(1000)
-        rgb.showColor(neopixel.colors(NeoPixelColors.Violet))
-        basic.pause(1000)
-        rgb.showColor(neopixel.colors(NeoPixelColors.White))
-        basic.pause(1000)
-    } else {
-        rgb.showColor(neopixel.colors(NeoPixelColors.Black))
+    if (rgbEstaEncendido == 0) {
+        if (input.lightLevel() <= 55) {
+            rgb.showColor(neopixel.colors(NeoPixelColors.Red))
+            basic.pause(1000)
+            rgb.showColor(neopixel.colors(NeoPixelColors.Orange))
+            basic.pause(1000)
+            rgb.showColor(neopixel.colors(NeoPixelColors.Yellow))
+            basic.pause(1000)
+            rgb.showColor(neopixel.colors(NeoPixelColors.Green))
+            basic.pause(1000)
+            rgb.showColor(neopixel.colors(NeoPixelColors.Blue))
+            basic.pause(1000)
+            rgb.showColor(neopixel.colors(NeoPixelColors.Indigo))
+            basic.pause(1000)
+            rgb.showColor(neopixel.colors(NeoPixelColors.Violet))
+            basic.pause(1000)
+            rgb.showColor(neopixel.colors(NeoPixelColors.White))
+            basic.pause(1000)
+        } else {
+            rgb.showColor(neopixel.colors(NeoPixelColors.Black))
+        }
     }
 })
